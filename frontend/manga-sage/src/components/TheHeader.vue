403fb72ff4a55ccae5d30cc5a40bbcd1c81e3e14
Bril3d <khaledtbk@gmail.com>
2023-05-12 18:54:02 +0100

    Header to Arabic

--- frontend/manga-sage/src/components/TheHeader.vue
+++ frontend/manga-sage/src/components/TheHeader.vue
@@ -7,7 +7,7 @@
       aria-label="Global"
     >
       <div class="flex items-center justify-between">
-        <router-link class="flex-none text-xl font-semibold text-white" to="/" aria-label="Brand"
+        <router-link class="flex-none text-xl font-semibold text-white" to="/" aria-label="MangaSage"
           >MangaSage</router-link
         >
         <div class="sm:hidden">
@@ -51,10 +51,9 @@
         <div
           class="flex flex-col gap-y-4 gap-x-0 mt-5 sm:flex-row sm:items-center sm:justify-end sm:gap-y-0 sm:gap-x-7 sm:mt-0 sm:pl-7"
         >
-          <a class="font-medium text-white sm:py-6" href="#" aria-current="page">Feed</a>
-          <a class="font-medium text-white/[.8] hover:text-white sm:py-6" href="#">Account</a>
-          <a class="font-medium text-white/[.8] hover:text-white sm:py-6" href="#">Work</a>
-          <a class="font-medium text-white/[.8] hover:text-white sm:py-6" href="#">Blog</a>
+          <a class="font-medium text-white sm:py-6" href="#" aria-current="page">الرئيسية</a>
+          <a class="font-medium text-white/[.8] hover:text-white sm:py-6" href="#">عشوائي</a>
+          <a class="font-medium text-white/[.8] hover:text-white sm:py-6" href="#">توصيات</a>
           <router-link :to="{ name: 'login' }">
             <a
               class="flex items-center gap-x-2 font-medium text-white/[.8] hover:text-white sm:border-l sm:border-white/[.3] sm:my-6 sm:pl-6"
@@ -72,7 +71,7 @@
                   d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"
                 />
               </svg>
-              Log in
+              تسجيل الدخول
             </a>
           </router-link>
           <router-link :to="{ name: 'register' }">
@@ -95,7 +94,7 @@
                   d="M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z"
                 />
               </svg>
-              Register
+              التسجيل
             </a>
           </router-link>
         </div>
--- frontend/manga-sage/src/pages/manga/SeriesView.vue
+++ frontend/manga-sage/src/pages/manga/SeriesView.vue
@@ -1,9 +1,13 @@
 <template>
     <FeedLayout>
-        <div class="mt-3 grid grid-cols-2 grid-rows-4">
-            <div class="dark:border-gray-700">
-                
-            </div>
+        <div class="py-14 px-10 grid grid-cols-3 grid-rows-4">
+            <div class="flex flex-col justify-center items-center gap-5 dark:shadow-slate-700/[.7] shadow-sm rounded-xl pb-5">
+            <img
+        class="w-full h-96 rounded-t-xl object-cover overflow-hidden"
+        src="/src/assets/manga/Apotheosis/cover.jpg"
+      />
+      <h3 dir="ltr" class="text-xl font-bold text-gray-800 dark:text-white truncate">Apotheosis</h3>
+    </div>        
         </div>
     </FeedLayout>
 </template>
