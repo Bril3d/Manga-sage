0021d2791363c10f42057a51195343adc6161bea
Bril3d <khaledtbk@gmail.com>
2023-05-13 23:05:34 +0100

    Fixing Mobile Grid

--- frontend/manga-sage/src/pages/FeedView.vue
+++ frontend/manga-sage/src/pages/FeedView.vue
@@ -3,7 +3,7 @@
     <HeroSlider class="drop-shadow-xl" />
     <BestOfView />
     <div
-      class="grid grid-flow-row gap-8 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-10"
+      class="grid grid-flow-row gap-8 text-neutral-600 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-10"
     >
 
       <div v-if="Manga.length > 0" v-for="m in Manga">
